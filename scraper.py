631465ff807a76e3b6a4a0abf5ce34978c3413e2
Ella Svete <ella.svete@gmail.com>
2022-05-10 18:46:58 -0700

    finished

--- scraper.py
+++ scraper.py
@@ -4,10 +4,33 @@ from bs4 import BeautifulSoup
 
 
 
+base_url = "https://en.wikipedia.org/wiki/History_of_Mexico"
+page = requests.get(base_url)
+
+
 def get_citations_needed_count():
-  pass
+  soup = BeautifulSoup(page.content, "html.parser")
+  results = soup.find_all(title = "Wikipedia:Citation needed")
+  citations_count = 0
+  
+  for result in results:
+    citations_count += 1
+
+  print(f"This page: {base_url} needs {citations_count} citations")
 
 
 
 def get_citations_needed_report():
-  pass
+  soup = BeautifulSoup(page.content, "html.parser")
+  results = soup.find_all(title = "Wikipedia:Citation needed")
+  citations_needed = []
+
+  for result in results:
+    citations_needed.append(result.parent.parent.parent.text)
+
+  for paragraph in citations_needed:
+    print(paragraph)
+
+
+get_citations_needed_count()
+get_citations_needed_report()
